241fb3fc4b5fed088d9ca937c2985b32a8958435
marcamaudruz <marc.amaudruz@gmail.com>
2023-12-24 08:27:45 +0100

    done

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from '../../styles/Ninjas.module.css'
 
 export const getStaticProps = async () => {
@@ -14,11 +15,9 @@ const Ninjas = ({ ninjas }) => {
         <div>
             <h1>All Ninjas</h1>
             {ninjas.map(ninja => (
-                <div className={styles.single} key={ninja.id}>
-                    <a>
-                        <h3>{ ninja.name }</h3>
-                    </a>
-                </div>
+                <Link href={ '/ninjas/' + ninja.id} className={styles.single} key={ninja.id}>
+                    <h3>{ ninja.name }</h3>
+                </Link>
             ))}
         </div>
      );
